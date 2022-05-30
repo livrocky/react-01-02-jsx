98d63bbcec5df71a607aca2fe1c3aec4f360adec
rokas <rokasra@gmail.com>
2022-05-30 12:03:57 +0300

    class notes (new york card)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
-// import './App.css';
+import './app.css';
+import img1 from './newyork.jpg';
+const username = 'James';
+
+const card = (
+  <article className='card'>
+    <img className='card-image' src={img1} alt='Card' />
+    <div className='card-content'>
+      <h3>paris</h3>
+      <p className='date'>{new Date().toDateString()}</p>
+      <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+    </div>
+  </article>
+);
+
+function add(n1, n2) {
+  return n1 + n2;
+}
 
 function App() {
-  return <h1>Hello react world!!!</h1>;
+  return (
+    <div>
+      <section>
+        <article className='card'>
+          <img className='card-image' src={img1} alt='Card' />
+          <div className='card-content'>
+            <h3>New york</h3>
+            <p className='date'>{new Date().toDateString()}</p>
+            <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+          </div>
+        </article>
+      </section>
+    </div>
+  );
 }
 
 export default App;
