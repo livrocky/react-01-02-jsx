9224a79f9c5b1e4023f23bbd7a9e3c0de03c0760
rokas <rokasra@gmail.com>
2022-05-31 09:00:38 +0300

    reusable components

--- src/App.js
+++ src/App.js
@@ -1,32 +1,38 @@
 import './app.css';
 import img1 from './newyork.jpg';
-const username = 'James';
 
-const card = (
-  <article className='card'>
-    <img className='card-image' src={img1} alt='Card' />
-    <div className='card-content'>
-      <h3>paris</h3>
-      <p className='date'>{new Date().toDateString()}</p>
-      <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
-    </div>
-  </article>
-);
+// const card = (
+//   <article className='card'>
+//     <img className='card-image' src={img1} alt='Card' />
+//     <div className='card-content'>
+//       <h3>paris</h3>
+//       <p className='date'>{new Date().toDateString()}</p>
+//       <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+//     </div>
+//   </article>
+// );
 
-function add(n1, n2) {
-  return n1 + n2;
+// function add(n1, n2) {
+//   return n1 + n2;
+// }
+
+function MyBtn(props) {
+  console.log('props===', props);
+  return <button className='btn-black'>{props.text}</button>;
 }
 
 function App() {
   return (
     <div>
       <section>
+        <MyBtn text='I am reusable component' />
         <article className='card'>
           <img className='card-image' src={img1} alt='Card' />
           <div className='card-content'>
             <h3>New york</h3>
             <p className='date'>{new Date().toDateString()}</p>
             <p className='text'>Praesent tincidunt sed tellus ut rutrum sed vitae justo.</p>
+            <MyBtn text='Buy tickets' />
           </div>
         </article>
       </section>
